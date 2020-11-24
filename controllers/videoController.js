b8de098fc21930562fd3e4cf2f6f5dadd581b418
dbarraca <daniel.barraca@gmail.com>
2020-11-23 22:12:43 -0800

    Created helper function for renaming uploads

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,7 +10,7 @@ var images = ['./images/image.png'];
 
 var videoOptions = {
     fps: 25,
-   loop: 10, // seconds
+    loop: 300, // seconds
     // transition: true,
     // transitionDuration: , // seconds
     videoBitrate: 1024,
@@ -41,11 +41,82 @@ exports.video_get = function(req, res) {
     });
 };
 
-// Handle Video create on POST.
+// Handle default video create on POST.
 exports.video_create_get =  function(req, res) {
     res.sendFile(path.join(__dirname + '/../createVideo.html'));
 };
 
+/*
+var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+})
+*/
+
+function getExtension(filename) {
+    let filenameArr = filename.split(".");
+
+    return filenameArr[filenameArr.length - 1];
+}
+
+function renameUpload(input, req) {
+    console.log("Path: " + req.files[input][0].path);
+    console.log("Originalname: " + req.files[input][0].originalname);
+    console.log("Extension: " + getExtension(req.files[input][0].originalname));
+
+    var Path = req.files[input][0].path + getExtension(req.files[input][0].originalname);
+
+    fs.rename(req.files[input][0].path, Path, (err) => {
+        if (err) throw err;
+        console.log(input + ' rename complete!');
+    });
+
+    return Path;
+}
+
+//Handle deafult video create on POST with audio and image
+exports.video_create_post = 
+[
+    upload.fields(
+        [
+            { name: 'image', maxCount: 1 }, 
+            { name: 'audio', maxCount: 1 }
+        ]
+    ),
+    function (req, res, next) {
+        // req.file is the `audio` file
+        // req.body will hold the text fields, if there were any
+
+        var backgroundImage = [renameUpload('image', req)];
+
+        videoshow(backgroundImage, videoOptions)
+        .audio(renameUpload('audio', req))
+        .save('./videos/' + req.body.videoFilename + '.mp4')
+        .on('start', function (command) { 
+            console.log("starting videoshow");
+            console.log('ffmpeg process started:', command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            console.error('Error:', err)
+            console.error('ffmpeg stderr:', stderr)
+        })
+        .on('end', function (output) {
+            console.error('Video created in:', output);
+            // res.sendFile(path.join(__dirname + '/index.html'));
+            res.download(output);
+        });
+    }
+];
+
+
+// Handle default video create on POST with image
+/*
 exports.video_create_post = [upload.single('image'), function (req, res, next) {
     // req.file is the `audio` file
     // req.body will hold the text fields, if there were any
@@ -84,6 +155,7 @@ exports.video_create_post = [upload.single('image'), function (req, res, next) {
         res.download(output);
     });
 }];
+*/
 
 /*
 var cpUpload = upload.fields([{ name: 'videoFilename', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
